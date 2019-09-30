734eedd72822569f6f549aea6924b6e40b2985ae
Summer <summer.eisertw@gmail.com>
2019-09-29 20:23:07 -0600

    Functioning navigation, index - menu - gallery done

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const pageIds = {
   menuPage: "menu-content",
+  menuButton: "menu-button",
   eventsPage: "events-content",
+  eventsButton: "events-button",
   galleryPage: "gallery-content",
+  galleryButton: "gallery-button",
   contactPage: "contact-content",
-  reservationsPage: "reservation-content"
+  contactButton: "contact-button",
+  reservationPage: "reservation-content",
+  reservationButton: "reservation-button"
 };
 const displayClass = "display-none";
 
@@ -15,11 +20,12 @@ class HtmlHelper {
     element.classList.remove(displayClass);
   }
 }
+const htmlHelper = new HtmlHelper()
 
-function onLoad(pageId) {
+function showPage(pageId) {
   // Purpose: to execute on the load of the page and hide any pages that should be hidden based on the url, and show any pages that should be shown.
   // PageObjects
-  for (var i = 0; i < PageObjects.length - 1; i++) {
+  for (var i = 0; i < PageObjects.length; i++) {
     var pageObject = PageObjects[i];
     pageObject.shouldShow(pageId);
   }
@@ -28,9 +34,13 @@ function onLoad(pageId) {
 }
 
 class Page {
-  constructor(pageId) {
+  constructor(pageId, pageButtonId) {
     this.pageId = pageId;
     this.pageElement = document.getElementById(this.pageId);
+    this.pageButtonElement = document.getElementById(pageButtonId);
+    this.pageButtonElement.addEventListener("click", () => {
+      showPage(this.pageId)
+    });
   }
 
   shouldShow(pageId) {
@@ -39,34 +49,37 @@ class Page {
     }
     // If object id is equal to passed in id, remove hide classList
     if (this.pageId == pageId) {
-      HtmlHelper.show(this.pageElement);
+      htmlHelper.show(this.pageElement);
     }
     // else add hide class to classList
     else {
-      HtmlHelper.hide(this.pageElement);
+      htmlHelper.hide(this.pageElement);
     }
     return true;
   }
 }
 
 const PageObjects = [
-  new Page(pageIds.menuPage),
-  new Page(pageIds.eventsPage),
-  new Page(pageIds.galleryPage),
-  new Page(pageIds.contactPage),
-  new Page(pageIds.reservationsPage)
+  new Page(pageIds.menuPage, pageIds.menuButton),
+  new Page(pageIds.eventsPage, pageIds.eventsButton),
+  new Page(pageIds.galleryPage, pageIds.galleryButton),
+  new Page(pageIds.contactPage, pageIds.contactButton),
+  new Page(pageIds.reservationPage, pageIds.reservationButton)
 ];
 
-function getCurrentPage() {
-  var url = document.URL;
-  // If the string ends with a /, remove it
-  if (url && url.lastIndexOf("/") == url.length - 1) {
-    url = url.slice(0, url.length - 1);
-  }
-  var lastSlashIndex = url.lastIndexOf("/");
-  var pageId = url.slice(lastSlashIndex, url.length - 1)
-  console.log(pageId)
-}
-getCurrentPage()
-//print(document.URL)
-currentPage = document.URL;
+showPage(pageIds.reservationPage);
+
+
+// function getCurrentPage() {
+//   var url = document.URL;
+//   // If the string ends with a /, remove it
+//   if (url && url.lastIndexOf("/") == url.length - 1) {
+//     url = url.slice(0, url.length - 1);
+//   }
+//   var lastSlashIndex = url.lastIndexOf("/");
+//   var pageId = url.slice(lastSlashIndex, url.length - 1)
+//   console.log(pageId)
+// }
+// getCurrentPage()
+// //print(document.URL)
+// currentPage = document.URL;
